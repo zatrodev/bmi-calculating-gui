296eb566b5e5c85f9c2f255f47578a0036bbf58c
zatro <zatroboy@gmail.com>
2023-03-29 16:54:19 +0800

    changed firebase to sqlite

---
+++ db/sql_db.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+from db.user_info import UserInfo
+
+
+class DatabaseService:
+    def __init__(self):
+        self.conn = sqlite3.connect('users.db')
+        self.c = self.conn.cursor()
+        self.c.execute('''CREATE TABLE IF NOT EXISTS users
+                    (name TEXT, age INTEGER, lrn TEXT, weight REAL, height REAL)''')
+
+    def insert_user(self, user):
+        self.c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)",
+                       (user.name, user.age, user.lrn, user.weight, user.height))
+        self.conn.commit()
+
+    def get_user_by_lrn(self, lrn):
+        self.c.execute("SELECT * FROM users WHERE lrn=?", (lrn,))
+
+        result = self.c.fetchone()
+
+        if result:
+            user = UserInfo(result[0], result[1],
+                            result[2], result[3], result[4])
+            return user
+        else:
+            return None
+
+    def close(self):
+        self.conn.close()
+
+
+# service = DatabaseService()
+# user1 = service.get_user_by_lrn("12345678")
+
+# if user1:
+#     print(user1.weight)
+# else:
+#     print("Invalid user")
--- interface/user_info_display.py
+++ interface/user_info_display.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-from db.firebase import DatabaseService
+from db.sql_db import DatabaseService
 
 
 class UserInfoDisplay(tk.Frame):
@@ -10,11 +10,11 @@ class UserInfoDisplay(tk.Frame):
         self.service = DatabaseService()
 
         self.lrn_entry = tk.Entry(self)
-        self.lrn_entry.grid(row=0, column=1, padx=5, pady=5)
+        self.lrn_entry.grid(row=0, column=0, padx=5, pady=5)
 
         search_button = ttk.Button(
             self, text="Search User", command=self.search_user)
-        search_button.grid(row=1, column=1, padx=5, pady=5)
+        search_button.grid(row=1, column=0, padx=5, pady=5)
 
         self.name_label = tk.Label(self, text="")
         self.name_label.grid(row=4, column=0, padx=5, pady=5)
@@ -22,20 +22,17 @@ class UserInfoDisplay(tk.Frame):
         self.age_label = tk.Label(self, text="")
         self.age_label.grid(row=5, column=0, padx=5, pady=5)
 
-        self.lrn_label = tk.Label(self, text="")
-        self.lrn_label.grid(row=6, column=0, padx=5, pady=5)
-
         self.weight_label = tk.Label(self, text="")
-        self.weight_label.grid(row=7, column=0, padx=5, pady=5)
+        self.weight_label.grid(row=6, column=0, padx=5, pady=5)
 
         self.height_label = tk.Label(self, text="")
-        self.height_label.grid(row=8, column=0, padx=5, pady=5)
+        self.height_label.grid(row=7, column=0, padx=5, pady=5)
 
         self.bmi_label = tk.Label(self, text="")
-        self.bmi_label.grid(row=9, column=0, padx=5, pady=5)
+        self.bmi_label.grid(row=8, column=0, padx=5, pady=5)
 
         self.nutritional_guide_label = tk.Label(self, text="")
-        self.nutritional_guide_label.grid(row=10, column=0, padx=5, pady=5)
+        self.nutritional_guide_label.grid(row=9, column=0, padx=5, pady=5)
 
         # name_label.pack()
         # age_label.pack()
@@ -48,25 +45,26 @@ class UserInfoDisplay(tk.Frame):
     def search_user(self):
         lrn = self.lrn_entry.get()
 
-        user_info = self.service.get_data_from_reference("/{}".format(lrn))
+        user_info = self.service.get_user_by_lrn(str(lrn))
 
-        self.name_label.config(text="Name: " +
-                               user_info["name"], font=("Arial", 24))
+        if user_info:
+            self.name_label.config(text="Name: " +
+                                   user_info.name, font=("Arial", 24))
 
-        self.age_label.config(text="Age: " +
-                              str(user_info["age"]), font=("Arial", 24))
+            self.age_label.config(text="Age: " +
+                                  str(user_info.age), font=("Arial", 24))
 
-        self.lrn_label.config(text="LRN: " +
-                              str(user_info["lrn"]), font=("Arial", 24))
+            self.weight_label.config(
+                text="Weight (kg): " + str(user_info.weight), font=("Arial", 24))
 
-        self.weight_label.config(
-            text="Weight (kg): " + str(user_info["weight"]), font=("Arial", 24))
+            self.height_label.config(
+                text="Height (m): " + str(user_info.height), font=("Arial", 24))
 
-        self.height_label.config(
-            text="Height (m): " + str(user_info["height"]), font=("Arial", 24))
+            self.bmi_label.config(
+                text="BMI: {:.2f} ({})".format(user_info.bmi, user_info.classification), font=("Arial", 24))
 
-        self.bmi_label.config(
-            text="BMI: {:.2f} ({})".format(user_info['bmi'], user_info['classification']), font=("Arial", 24))
-
-        self.nutritional_guide_label.config(
-            text=user_info["nutritional_guide"], font=("Arial Bold", 24))
+            self.nutritional_guide_label.config(
+                text=user_info.nutritional_guide, font=("Arial Bold", 24))
+        else:
+            self.name_label.config(text="Invalid user with lrn \"{}\"".format(
+                lrn), font=("Arial", 24), fg="#f00")
