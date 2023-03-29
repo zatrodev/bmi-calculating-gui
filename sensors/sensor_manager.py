698c50f9ca379ee73dff8b0746899391ed977955
zatro <zatroboy@gmail.com>
2023-03-29 22:51:59 +0800

    bug fix

--- sensors/sensor_manager.py
+++ sensors/sensor_manager.py
@@ -2,10 +2,10 @@ from sensors.ultrasonic_sensor import get_distance
 
 
 class SensorManager:
-    @classmethod
+    @staticmethod
     def get_height():
         return get_distance()
 
-    @classmethod
+    @staticmethod
     def get_weight():
         return 60
