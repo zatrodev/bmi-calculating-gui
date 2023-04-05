2a070183ded4155bfaa5f06bbb24bbfe102d8a73
Alexa Garcia <garcia.alexa.0738@gmail.com>
2023-04-05 11:51:01 +0800

    cnjscn

--- sensors/pressure_sensor.py
+++ sensors/pressure_sensor.py
@@ -5,11 +5,11 @@ from pyA20.gpio import port
 try:
     hx711 = HX711(dout_pin=port.PA12, pd_sck_pin=port.PA11)
 
-    err = hx711.zero()
-    if err:
-        raise ValueError('Tare is unsuccessful.')
+    # err = hx711.zero()
+    # if err:
+    #     raise ValueError('Tare is unsuccessful.')
 
     while True:
-        print(hx711.get_raw_data_mean())
+        print(hx711.get_weight_mean())
 except (KeyboardInterrupt, SystemExit):
     print("Exiting...")
