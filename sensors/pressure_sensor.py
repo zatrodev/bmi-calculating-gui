24ba626e1323df849637354707037d999fe8fcd1
Alexa Garcia <garcia.alexa.0738@gmail.com>
2023-04-05 09:54:41 +0800

    updated hx711

--- sensors/pressure_sensor.py
+++ sensors/pressure_sensor.py
@@ -1,17 +1,14 @@
-from pyA20.gpio import gpio
-from pyA20.gpio import port
-from pyA20.gpio import connector
 from hx711 import HX711
 
-# Set up the HX711 sensor
-hx711 = HX711(dout=port.PA12, pd_sck=port.PA11)
-print("init")
-hx711.set_reading_format("MSB", "MSB")
-hx711.set_reference_unit(1)
-hx711.reset()
 
-# Read the weight
-weight = hx711.get_weight()
+try:
+    hx711 = HX711(dout_pin=12, pd_sck_pin=11)
 
-# Print the weight
-print("Weight: {} grams".format(weight))
+    err = hx711.zero()
+    if err:
+        raise ValueError('Tare is unsuccessful.')
+
+    while True:
+        print(hx711.get_raw_data_mean())
+except (KeyboardInterrupt, SystemExit):
+    print("Exiting...")
